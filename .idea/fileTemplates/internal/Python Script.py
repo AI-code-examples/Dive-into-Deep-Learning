816ce5a7e0b3a81c1145f65439d645b513649876
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-27 10:35:35 +0800

    Ch02.Sec02.PyCharm.Config.End

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   Dive-into-Deep-Learning
+@File       :   sec0202.py
+@Version    :   v0.1
+@Time       :   2020-12-27 9:25
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :   《动手学深度学习》
+@Desc       :   Sec
+@理解：
+"""
+from mxnet import nd
+from tools import beep_end
+
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+def main():
+    pass
+
+
+# ----------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    # 运行结束的提醒
+    main()
+    tools.beep_end()
