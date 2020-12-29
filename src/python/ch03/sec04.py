55bb76e7217cea4213b5a301fb15d9cb6fc981e7
zYx.Tom <zhuyuanxiang@gmail.com>
2020-12-29 16:02:52 +0800

    Ch03.Sec04.End

---
+++ src/python/ch03/sec04.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   Dive-into-Deep-Learning
+@File       :   sec0202.py
+@Version    :   v0.1
+@Time       :   2020-12-27 9:25
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :   《动手学深度学习》
+@Desc       :   Sec 3.4 Softmax 回归
+@小结：
+1.  Softmax 回归适用于分类问题，使用 Softmax 函数输出类别的概率分布
+2.  Softmax 回归是一个单层神经网络，输出个数等于分类问题中的类别个数
+3.  交叉熵适合衡量两个概率分布的差异
+"""
+from mxnet import nd
+from tools import beep_end, show_subtitle
+
+
+# ----------------------------------------------------------------------
+def main():
+    pass
+
+
+# ----------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    main()
+    # 运行结束的提醒
+    beep_end()
