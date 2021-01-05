e5c99d54f02f8e2ac8643670b76b54860afae3de
zYx.Tom <zhuyuanxiang@gmail.com>
2021-01-05 18:07:34 +0800

    d2l.load_data_jay_lyrics() 函数无法正确的取得数据文件的地址，因此我就重写了个

---
+++ data.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-    
+"""
+@Author     :   zYx.Tom
+@Contact    :   526614962@qq.com
+@site       :   https://zhuyuanxiang.github.io
+---------------------------
+@Software   :   PyCharm
+@Project    :   Dive-into-Deep-Learning
+@File       :   sec0202.py
+@Version    :   v0.1
+@Time       :   2020-12-27 9:25
+@License    :   (C)Copyright 2018-2020, zYx.Tom
+@Reference  :   《动手学深度学习》
+@Desc       :   Sec
+@小结：
+"""
+
+import d2lzh as d2l
+import mxnet as mx
+import zipfile
+from mxnet import autograd, gluon, init, nd
+from mxnet.gluon import data as gdata, loss as gloss, nn
+from tools import beep_end, show_subtitle, show_title, show_figures, get_root_path
+
+
+# ----------------------------------------------------------------------
+def main():
+    pass
+
+
+def load_data_jay_lyrics():
+    """Load the Jay Chou lyric data set (available in the Chinese book)."""
+    with zipfile.ZipFile(get_root_path() + 'data/jaychou_lyrics.txt.zip') as zin:
+        with zin.open('jaychou_lyrics.txt') as f:
+            corpus_chars = f.read().decode('utf-8')
+    corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
+    corpus_chars = corpus_chars[0:10000]
+    idx_to_char = list(set(corpus_chars))
+    char_to_idx = dict([(char, i) for i, char in enumerate(idx_to_char)])
+    vocab_size = len(char_to_idx)
+    corpus_indices = [char_to_idx[char] for char in corpus_chars]
+    return corpus_indices, char_to_idx, idx_to_char, vocab_size
+
+
+# ----------------------------------------------------------------------
+
+
+if __name__ == '__main__':
+    main()
+    # 运行结束的提醒
+    beep_end()
+    show_figures()
